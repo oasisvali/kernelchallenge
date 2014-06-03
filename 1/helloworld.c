3cd8f8d05ab07259e81b6acae76a557391807cd1
Oasis Vali <oasis.vali@gmail.com>
2014-06-03 12:28:23 -0500

    Created module file and makefile

---
+++ 1/helloworld.c
@@ -0,0 +1,38 @@
+/*  helloworld.c - The simplest kernel module.
+ **
+ **  Copyright (C) 2001 by Peter Jay Salzman
+ **
+ **  08/02/2006 - Updated by Rodrigo Rubira Branco <rodrigo@kernelhacking.com>
+ **/
+
+/* Kernel Programming */
+#define MODULE
+#define LINUX
+#define __KERNEL__
+
+#include <linux/module.h>  /* Needed by all modules */
+#include <linux/kernel.h>  /* Needed for KERN_ALERT */
+#include <linux/init.h>    /* Needed for macros */
+
+/* Module Info */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Oasis Vali");
+MODULE_DESCRIPTION("A simple Hello World module");
+
+static int init(void)
+{
+  printk(KERN_ALERT "Hello world!\n");
+      
+  // A non 0 return means init_module failed; module can't be loaded.
+  return 0;
+}
+        
+static void exit(void)
+{
+  printk(KERN_ALERT "Goodbye world!\n");
+}
+
+/* Registering init and cleanup functions */
+module_init(init);
+module_exit(exit);
+
